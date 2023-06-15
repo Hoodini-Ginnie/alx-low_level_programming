ab0b5cfb992cee6906aace3db861d9f994515b0c
Hoodini-Ginnie <mwasseddie005@gmail.com>
2023-06-15 10:59:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,18 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * Return: Always 0.
+ * main -Entry point
+ * 
+ * Return: Always 0 (Success)
  */ 
 
 
 int main(void)
 
 {
+	
 	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
+
 }
