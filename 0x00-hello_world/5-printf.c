afdae98549977527d339ba17643ccbfd9f204c74
Hoodini-Ginnie <mwasseddie005@gmail.com>
2023-06-15 12:59:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * Main - Entry point.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art, In");
+	return (0);
+}
