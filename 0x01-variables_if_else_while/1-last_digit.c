c6d172b9c9bdf6a2f8438c32c5bee10301073099
Hoodini-Ginnie <mwasseddie005@gmail.com>
2023-06-17 07:08:31 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,8 @@
 /**
  * main - entry point
  *
- * Description: a
+ * Description: print the value of n status:
+ *		greater than, is zero and is not less than 6
  *
  * Return:Always 0 (success)
 */
