984ae05a63545bdc70a70941366e373755332da8
Hoodini-Ginnie <mwasseddie005@gmail.com>
2023-06-20 11:21:10 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last _digit - Prints the last digit of a number
- * @n: The int to extract the last digit
+ * @c: The int to extract the last digit
  * Return: Value of the last digit of number
  */
 
