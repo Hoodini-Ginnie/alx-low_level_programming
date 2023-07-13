1fe5e1e6d7eb251741fe73963bd7981c7a365c69
Hoodini-Ginnie <mwasseddie005@gmail.com>
2023-07-13 08:36:04 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,31 +4,40 @@
 
 /**
  * realloc - reallocates a memory block
- * @ptr: printer to the memory previously allocated
+ * @ptr: pointer to the memory allocated with malloc(old_size)
  * @old_size: size in bytes of the allocated space of ptr
  * @new_size: new size, in bytes of the new memory block
- * Return: ptr
- * if new size == old_size, returns ptr witout changes
- * if malloc fails, returns NULL
+ * Return: NULL if new_size = 0 and ptr is not NULL
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 {
+	void *p;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (ptr == NULL)
-		ptr = malloc(new_size);
-
-	if (new_size == old_size)
-		return (ptr);
-
-	free(ptr);
-	ptr = malloc(new_size);
-
-	return (ptr);
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < old_size && i < new_size; i++)
+			*((char *)p + 1) = *((char *)ptr + 1);
+		free(ptr);
+	}
+	return (p);
 }
